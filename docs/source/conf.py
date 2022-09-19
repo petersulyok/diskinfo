5ec3179d550b7692af079bb83248f17ae9954390
petersulyok <peter@sulyok.net>
2022-09-19 12:13:36 +0200

    Module not found error fixed.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,18 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src/disk_info/'))
+import sphinx_rtd_theme
+
 project = 'disk_info'
 copyright = '2022, Peter Sulyok'
 author = 'Peter Sulyok'
-release = '0.9.1'
+release = '0.9.2'
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -20,6 +28,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.napoleon",
+    "sphinx_copybutton",
 ]
 
 templates_path = ['_templates']
@@ -28,7 +37,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 intersphinx_mapping = {"python": ("http://docs.python.org/3", None)}
 autodoc_typehints = "description"
