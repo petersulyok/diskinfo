b440aacd57ea020b1558daa0e7c12855543f6db2
petersulyok <peter@sulyok.net>
2022-09-20 20:48:50 +0200

    Ric demo demo implemented.

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # diskinfo
-[![Documentation Status](https://readthedocs.org/projects/diskinfo/badge/?version=latest)](https://disk-info.readthedocs.io/en/latest/?badge=latest)
+[![Documentation Status](https://readthedocs.org/projects/diskinfo/badge/?version=latest)](https://diskinfo.readthedocs.io/en/latest/?badge=latest)
 
-A disk information Python library for Linux.
+Disk information Python library for Linux.
 
-This library provides API for:
-1. collecting information about a specific disk
-2. discovering existing disks in your system
-3. translation between device names and persistent names, for example:
+Demo
+----
+A simple demo is provided in the package:
 
-       "/dev/sda" <-> "/dev/disk/by-id/ata-Samsung_SSD_850_PRO_1TB_S3D2NY0J819218R" 
+<img src="https://github.com/petersulyok/diskinfo/raw/main/docs/diskinfo_rich_demo.png" align="center">
 
+(for this colorful demo the ``rich`` Python library needs to be installed, but this is optional)
 Installation
 ------------
-Standard installation from `pypi`:
+Standard installation from `pypi` with any of your favourite tools:
 
     pip install diskinfo
 
@@ -21,40 +21,3 @@ The package does not have extra dependencies.
 API documentation
 -----------------
 The detailed [API documentation](https://diskinfo.readthedocs.io/en/latest/index.html) can be found on ``readthedocs.io``.
-
-Demo
-----
-In the package a short demo can be found. For running the demo, execute this:
-
-    python -m diskinfo.demo
-    There are 2 disks installed in this system (0 HDDs, 2 SSDs, 0 NVMEs).
-    [sda]
-        path:                     /dev/sda
-        model:                    Samsung SSD 850 PRO 1TB
-        size:                     953.9 GB
-        serial:                   S3D2NY0J819218R
-        firmware:                 EXM04B6Q
-        device type:              SSD
-        by-id path:               ['/dev/disk/by-id/wwn-0x5002539c407370be', '/dev/disk/by-id/ata-Samsung_SSD_850_PRO_1TB_S3D2NY0J819218R']
-        by-path path:             ['/dev/disk/by-path/pci-0000:00:17.0-ata-1.0', '/dev/disk/by-path/pci-0000:00:17.0-ata-1']
-        wwn id:                   0x5002539c407370be
-        device id:                (8:0)
-        Physical block size:      512
-        Logical block size:       512
-        Partition table type:     gpt
-        Partition table UUID:     008e4c54-96c9-4771-9e13-60dfe00ebb7f
-    [sdb]
-        path:                     /dev/sdb
-        model:                    Samsung SSD 850 EVO 2TB
-        size:                     1.8 TB
-        serial:                   S3HDNWBG801398X
-        firmware:                 EMT02B6Q
-        device type:              SSD
-        by-id path:               ['/dev/disk/by-id/ata-Samsung_SSD_850_EVO_2TB_S3HDNWBG801398X', '/dev/disk/by-id/wwn-0x5002538c8000a847']
-        by-path path:             ['/dev/disk/by-path/pci-0000:00:17.0-ata-2', '/dev/disk/by-path/pci-0000:00:17.0-ata-2.0']
-        wwn id:                   0x5002538c8000a847
-        device id:                (8:16)
-        Physical block size:      512
-        Logical block size:       512
-        Partition table type:     gpt
-        Partition table UUID:     d3f932e0-7107-455e-a569-9acd5b60d204
--- src/diskinfo/demo.py
+++ src/diskinfo/demo.py
@@ -3,6 +3,16 @@
 #    Peter Sulyok (C) 2022.
 #
 from diskinfo import DiskType, DiskInfo
+RICH = True
+# Import rich if installed
+try:
+    from rich import print as rprint
+    from rich.panel import Panel
+    from rich.table import Table
+    from rich import box
+    from rich.console import Group
+except ImportError:
+    RICH = False
 
 
 def main():
@@ -10,31 +20,62 @@ def main():
 
     # Discover disks in the system.
     di = DiskInfo()
+
     # Count number of the different disk types.
     disk_num = di.get_disk_number()
     hdd_num = di.get_disk_number(included={DiskType.HDD}, excluded={DiskType.NVME, DiskType.SSD})
     ssd_num = di.get_disk_number(included={DiskType.SSD}, excluded={DiskType.NVME, DiskType.HDD})
     nvme_num = di.get_disk_number(included={DiskType.NVME}, excluded={DiskType.HDD})
-    # Print the attributes of the discovered disks.
-    print(f"There are {disk_num} disks installed in this system ({hdd_num} HDDs, {ssd_num} SSDs, {nvme_num} NVMEs).")
-    disks = di.get_disk_list(sorting=True)
-    for d in disks:
-        print(f"[{d.get_name()}]")
-        print(f"\tpath:                     {d.get_path()}")
-        print(f"\tmodel:                    {d.get_model()}")
-        s, u = d.get_size_in_hrf(units=2)
-        print(f"\tsize:                     {s:.1f} {u}")
-        print(f"\tserial:                   {d.get_serial()}")
-        print(f"\tfirmware:                 {d.get_firmware()}")
-        print(f"\tdevice type:              {d.get_type_str()}")
-        print(f"\tby-id path:               {d.get_byid_path()}")
-        print(f"\tby-path path:             {d.get_bypath_path()}")
-        print(f"\twwn id:                   {d.get_wwn()}")
-        print(f"\tdevice id:                ({d.get_dev_id()})")
-        print(f"\tPhysical block size:      {d.get_physical_block_size()}")
-        print(f"\tLogical block size:       {d.get_logical_block_size()}")
-        print(f"\tPartition table type:     {d.get_partition_table_type()}")
-        print(f"\tPartition table UUID:     {d.get_partition_table_uuid()}")
+    verb = "are"
+    plural = "s"
+    if disk_num <= 1:
+        verb = "is"
+        plural = ""
+
+    # Rich demo.
+    if RICH:
+        panel = Panel(f"[markdown.strong]There {verb} [bold sky_blue2]{disk_num}[/] disk{plural} installed in this"
+                      f" system :point_right: [bold sky_blue2]{hdd_num}[/] HDDs, [bold sky_blue2]{ssd_num}[/]"
+                      f" SSDs, [bold sky_blue2]{nvme_num}[/] NVMEs[/]", box=box.MINIMAL, expand=False)
+        table = Table(border_style="gray30", box=box.MINIMAL)
+        table.add_column("Name", justify="left", style="bold orange1")
+        table.add_column("Type", justify="left", style="bold orchid")
+        table.add_column("Model", justify="left", style="bold gray54")
+        table.add_column("Path", justify="left", style="bold green")
+        table.add_column("Serial", justify="left", style="bold purple3")
+        table.add_column("Firmware", justify="left", style="bold slate_blue1")
+        table.add_column("Size", justify="right", style="bold blue")
+        disks = di.get_disk_list(sorting=True)
+        for d in disks:
+            s, u = d.get_size_in_hrf()
+            table.add_row(d.get_name(), d.get_type_str(), d.get_model(), d.get_path(), d.get_serial(),
+                          d.get_firmware(), f"{s:.1f} {u}")
+        group = Group(panel, table)
+        rprint(Panel(group, title="Discovery", title_align="left", border_style="gray30", expand=False))
+
+    # Normal demo.
+    else:
+        # Print the attributes of the discovered disks.
+        print(f"There {verb} {disk_num} disk{plural} installed in this system: {hdd_num} HDDs, {ssd_num} SSDs,"
+              f" {nvme_num} NVMEs")
+        disks = di.get_disk_list(sorting=True)
+        for d in disks:
+            print(f"[{d.get_name()}]")
+            print(f"\tpath:                     {d.get_path()}")
+            print(f"\tmodel:                    {d.get_model()}")
+            s, u = d.get_size_in_hrf(units=2)
+            print(f"\tsize:                     {s:.1f} {u}")
+            print(f"\tserial:                   {d.get_serial()}")
+            print(f"\tfirmware:                 {d.get_firmware()}")
+            print(f"\tdevice type:              {d.get_type_str()}")
+            print(f"\tby-id path:               {d.get_byid_path()}")
+            print(f"\tby-path path:             {d.get_bypath_path()}")
+            print(f"\twwn id:                   {d.get_wwn()}")
+            print(f"\tdevice id:                ({d.get_dev_id()})")
+            print(f"\tPhysical block size:      {d.get_physical_block_size()}")
+            print(f"\tLogical block size:       {d.get_logical_block_size()}")
+            print(f"\tPartition table type:     {d.get_partition_table_type()}")
+            print(f"\tPartition table UUID:     {d.get_partition_table_uuid()}")
 
 
 if __name__ == '__main__':
