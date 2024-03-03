0fd2ac02828369b756b2bdb35c006be867e93d37
petersulyok <peter@sulyok.net>
2024-03-03 20:35:04 +0100

    Change: version number updated.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'diskinfo'
 copyright = '2022-2024, Peter Sulyok'
 author = 'Peter Sulyok'
-release = '2.1.2'
+release = '3.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -30,6 +30,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_static_path = ['_static']
+#html_static_path = ['_static']
 intersphinx_mapping = {"python": ("http://docs.python.org/3", None)}
 autodoc_typehints = "description"
