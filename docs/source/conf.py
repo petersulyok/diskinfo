359fed522b75c72c7cbe6367f8a7a50f824b9073
petersulyok <peter@sulyok.net>
2024-03-10 22:27:42 +0100

    Changed: version updated to 3.1.0.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'diskinfo'
 copyright = '2022-2024, Peter Sulyok'
 author = 'Peter Sulyok'
-release = '3.0.0'
+release = '3.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
