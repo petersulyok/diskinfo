1d6ca9525f72d777285dce1f58ed2115efbfad47
petersulyok <peter@sulyok.net>
2026-02-20 07:07:32 +0100

    Refactored with pyudev.

--- src/diskinfo/__init__.py
+++ src/diskinfo/__init__.py
@@ -1,13 +1,13 @@
 #
 #    Package `diskinfo`
-#    Peter Sulyok (C) 2022-2024.
+#    Peter Sulyok (C) 2022-2026.
 #
 from diskinfo.disktype import DiskType
-from diskinfo.utils import _read_file, _read_udev_property, _read_udev_path, size_in_hrf, time_in_hrf
+from diskinfo.utils import _read_file, size_in_hrf, time_in_hrf, _pyudev_getint, _pyudev_getenc
 from diskinfo.partition import Partition
 from diskinfo.disksmart import DiskSmartData, SmartAttribute, NvmeAttributes
 from diskinfo.disk import Disk
 from diskinfo.diskinfo import DiskInfo
 
-__all__ = ["DiskType", "Partition", "DiskSmartData", "SmartAttribute", "NvmeAttributes", "Disk", "DiskInfo",
-           "_read_file", "_read_udev_property", "_read_udev_path", "size_in_hrf", "time_in_hrf"]
+__all__ = ['DiskType', 'Partition', 'DiskSmartData', 'SmartAttribute', 'NvmeAttributes', 'Disk', 'DiskInfo',
+           '_read_file', 'size_in_hrf', 'time_in_hrf', '_pyudev_getint', '_pyudev_getenc']
